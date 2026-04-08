7020b1266d27db7e3a694f742141c470e07917c9
peterrlorenzo <peterdave.lorenzo@tup.edu.ph>
2026-04-09 00:25:53 +0800

    push my sql

---
+++ project_sql/last_query.sql
@@ -0,0 +1,51 @@
+WITH skills_demand AS (
+    SELECT
+        skills_dim.skill_id,
+        skills_dim.skills,
+        COUNT(skills_job_dim.job_id) AS demand_count
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short IN ('Data Analyst', 'Data Engineer') AND
+        salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+), average_salary AS (
+    SELECT
+        skills_dim.skill_id,
+        ROUND(AVG(salary_year_avg), 2) AS avg_salary
+    FROM job_postings_fact
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+    WHERE
+        job_title_short IN ('Data Analyst', 'Data Engineer') AND
+        salary_year_avg IS NOT NULL
+    GROUP BY
+        skills_dim.skill_id
+)
+
+SELECT
+    skills_dim.skill_id,
+    skills_dim.skills,
+    COUNT(skills_job_dim.job_id) AS demand_count,
+    ROUND(AVG(job_postings_fact.salary_year_avg), 2) AS avg_salary
+FROM job_postings_fact
+INNER JOIN skills_job_dim
+    ON
+    job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim
+    ON
+    skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short IN ('Data Analyst', 'Data Engineer')
+    AND salary_year_avg IS NOT NULL
+    AND job_location = 'Anywhere'
+GROUP BY
+    skills_dim.skill_id
+HAVING
+    COUNT(skills_job_dim.job_id) > 10
+ORDER BY
+    avg_salary DESC,
+    demand_count DESC
+LIMIT 25;
